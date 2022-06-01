5f9103485fe8fb7924fe8fbd9238adaa2004b773
decosawa <62211442+andytxr@users.noreply.github.com>
2022-06-01 18:35:36 -0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+
+module.exports = {
+
+    entry: './ex/index.js',
+    output: {
+
+        path: __dirname + '/public',
+        filename: './bundle.js'
+
+    },
+    devServer: {
+
+        port: 3030,
+        contentBase: './public'
+
+    }
+
+}
